91342f882399a0e1e9773f07b819412db7cfc983
Cody Mudrack <kcardum@gmail.com>
2019-09-29 16:42:24 -0400

    Initial Commit

---
+++ InventoryProject/InventoryProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InventoryProject
+//
+//  Created by Cody Mudrack on 9/29/19.
+//  Copyright © 2019 Cody Mudrack. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
