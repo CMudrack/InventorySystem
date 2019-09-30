b48d08471fd9a0a1d2455a135e54f17428b8f2e6
Cody Mudrack <kcardum@gmail.com>
2019-09-29 20:28:46 -0400

    Added Table View for items

--- InventoryProject/InventoryProject/ViewController.swift
+++ InventoryProject/InventoryProject/ViewController.swift
@@ -8,13 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var CardTableView: UITableView!
+    let dataSet: [String] = ["Test", "Test 2", "Test 3", "Test 4"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataSet.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cardCell") as! CardCell
+        cell.configure(title: dataSet[indexPath.row])
+        return cell
+    }
 
 }
 
