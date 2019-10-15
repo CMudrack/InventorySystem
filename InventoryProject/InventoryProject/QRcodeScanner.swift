d92048981d7db5a09c0e0bc8c5785281134d7f26
Cody Mudrack <kcardum@gmail.com>
2019-10-15 12:33:23 -0400

    Added camera view to camera button

--- InventoryProject/InventoryProject/QRcodeScanner.swift
+++ InventoryProject/InventoryProject/QRcodeScanner.swift
@@ -6,20 +6,23 @@
 //  Copyright © 2019 Cody Mudrack. All rights reserved.
 //
 
-// This website helped me with this portion of the code: https://www.appcoda.com/barcode-reader-swift/
+// This website helped me with this portion of the code: https://developer.apple.com/documentation/avfoundation/cameras_and_media_capture/avcam_building_a_camera_app
 
 import UIKit
 import AVFoundation
 
 class QRcodeScanner: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
     
-    var captureSession: AVCaptureSession?
+    var captureSession = AVCaptureSession()
     var videoPreviewLayer: AVCaptureVideoPreviewLayer?
-    var qrCodeFrameView: UIView?
+    var camera: AVCaptureDevice?
+    var cameraCaptureOutput: AVCapturePhotoOutput?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+        //check for authorization
         switch AVCaptureDevice.authorizationStatus(for: .video) {
         case .authorized: // The user has previously granted access to the camera.
             self.setupCaptureSession()
@@ -46,46 +49,25 @@ class QRcodeScanner: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
     
     
     func setupCaptureSession() {
-    
-        // Get the back-facing camera for capturing videos
-        let deviceDiscoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInDualCamera], mediaType: AVMediaType.video, position: .back)
+        captureSession.sessionPreset = AVCaptureSession.Preset.high
+        camera = AVCaptureDevice.default(for: .video)
         
-        guard let captureDevice = deviceDiscoverySession.devices.first else {
-        print("Failed to get the camera device")
-        return
-        }
-        
-        do {
-        // Get an instance of the AVCaptureDeviceInput class using the previous device object.
-        let input = try AVCaptureDeviceInput(device: captureDevice)
-        
-        // Set the input device on the capture session.
-        captureSession?.addInput(input)
-        
-        
-        // Initialize a AVCaptureMetadataOutput object and set it as the output device to the capture session.
-        let captureMetadataOutput = AVCaptureMetadataOutput()
-        captureSession?.addOutput(captureMetadataOutput)
-        
-        // Set delegate and use the default dispatch queue to execute the call back
-        captureMetadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
-        captureMetadataOutput.metadataObjectTypes = [AVMetadataObject.ObjectType.qr]
-        
-        // Initialize the video preview layer and add it as a sublayer to the viewPreview view's layer.
-        videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession!)
-        videoPreviewLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
-        videoPreviewLayer?.frame = view.layer.bounds
-        view.layer.addSublayer(videoPreviewLayer!)
-        
-        // Start video capture.
-        captureSession?.startRunning()
-        
+        do{
+            let cameraCaptureInput = try AVCaptureDeviceInput(device: camera!)
+                cameraCaptureOutput = AVCapturePhotoOutput()
+            
+            captureSession.addInput(cameraCaptureInput)
+            captureSession.addOutput(cameraCaptureOutput!)
         } catch {
-            // If any error occurs, simply print it out and don't continue any more.
-            print(error)
-            return
+            print(error.localizedDescription)
         }
-       
+        
+        videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        videoPreviewLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        videoPreviewLayer?.frame = view.bounds
+        videoPreviewLayer?.connection?.videoOrientation = AVCaptureVideoOrientation.portrait
+        view.layer.insertSublayer(videoPreviewLayer!, at: 0)
+        captureSession.startRunning()
     }
     
 }
