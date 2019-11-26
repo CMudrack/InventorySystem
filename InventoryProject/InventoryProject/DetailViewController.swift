36e82e362d83e19c3dfc796e9b62bab66fbb8f50
Cody Mudrack <kcardum@gmail.com>
2019-11-26 10:11:29 -0500

    added delete button. doesn't work

--- InventoryProject/InventoryProject/DetailViewController.swift
+++ InventoryProject/InventoryProject/DetailViewController.swift
@@ -13,6 +13,7 @@ import UIKit
 class DetailViewController: UIViewController {
     
     let storage = Storage.storage(url: "gs://chemreserve-d952f.appspot.com").reference()
+    var ref: DatabaseReference! = Database.database().reference()
 
     var chemicalName: String? {
         didSet {
@@ -30,6 +31,22 @@ class DetailViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         chemNameText.text = chemicalName
         chemQuantityText.text = chemicalQuantity
+    }
+    
+    
+    @IBAction func deleteButtonPressed(_ sender: Any) {
+        
+        let noPDFAlert = UIAlertController(title: "Delete Item?", message: "Are you sure you want to delete this item?", preferredStyle: .alert)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        let deleteAction = UIAlertAction(title: "Delete", style: .destructive) { (action) in
+            self.ref.child("Chemicals").child(self.chemNameLabel.text!).setValue(nil)
+            DataStore.sharedInstance.itemAdded == true
+            self.navigationController?.popViewController(animated: true)
+        }
+        noPDFAlert.addAction(cancelAction)
+        noPDFAlert.addAction(deleteAction)
+        self.present(noPDFAlert, animated: true, completion: nil)
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
